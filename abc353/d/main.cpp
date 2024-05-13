b2a8b91268f0eb1ccff9ff8e76cf51a6b8d65bfd
myzk-a <rorosocksxion@gmail.com>
2024-05-14 08:05:37 +0900

    ABC353-D

--- abc353/d/main.cpp
+++ abc353/d/main.cpp
@@ -1,12 +1,14 @@
 #include <bits/stdc++.h>
+#include <atcoder/modint>
 using namespace std;
+using namespace atcoder;
 using ll = long long;
-const ll M = 998244353LL;
+using mint = atcoder::modint998244353;
 
 int main()
 {
-    vector<ll> pow_10;
-    ll v = 1;
+    vector<mint> pow_10;
+    mint v = 1;
     pow_10.push_back(v);
     for(ll i = 1; i <= 10; ++i)
     {
@@ -18,7 +20,7 @@ int main()
     cin >> N;
     vector<ll> A(N);
     for(auto &a : A) cin >> a;
-    map<ll, ll> cnt;
+    map<ll, mint> cnt;
     set<ll> key;
     for(auto a : A)
     {
@@ -29,14 +31,13 @@ int main()
         key.insert(keta);
     }
 
-    vector<ll> sum(N, 0);
+    vector<mint> sum(N, 0);
     sum[0] = A[0];
     for(ll i = 1; i < N; ++i)
     {
         sum[i] = sum[i-1] + A[i];
-        sum[i] %= M;
     } 
-    ll ans = 0;
+    mint ans = 0;
     for(ll i = 0; i < N; ++i)
     {
         ll a = A[i];
@@ -45,17 +46,12 @@ int main()
         --cnt[keta];
         for(auto k : key)
         {
-            ans += (((cnt[k] * a) % M) * (pow_10[k] % M )) % M;
-            ans %= M;
+            ans += cnt[k] * mint(a) * pow_10[k];
         }
-        ll tmp = sum[N - 1] - sum[i];
-        if(tmp < 0) tmp += M;
-        tmp %= M;
+        mint tmp = sum[N - 1] - sum[i];
         ans += tmp;
-        ans %= M;
     }
 
-    ans %= M;
-    std::cout << ans << std::endl;
+    std::cout << ans.val() << std::endl;
     return 0;
 }
